95004daee42c63a9d961afb6957fdbecc5bcf50c
Scooby <100179759+scoobymakesmethods@users.noreply.github.com>
2022-09-05 00:01:30 -0500

    Add files via upload

---
+++ req.bat
@@ -0,0 +1,8 @@
+pip install discord_webhook
+pip install pypiwin32
+pip install psutil
+pip install pyinstaller 
+pip install requests
+pip install Pillow
+pip install pycryptodomex
+pip install termcolor
